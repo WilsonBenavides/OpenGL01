04514bb901d3157c7abcd8687cc3f8b0fffb2491
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2020-05-07 23:37:12 -0500

    ep 04 Vertex Buffers and Drawing a Triangle in OpenGL

--- OpenGL01/src/Application.cpp
+++ OpenGL01/src/Application.cpp
@@ -22,13 +22,25 @@ int main(void)
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window); 
 
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
    
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+         -0.5f, -0.5f,
+          0.0f,  0.5f,
+          0.5f, -0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); 
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -36,11 +48,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
